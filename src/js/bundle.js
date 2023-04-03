5f6cf2ec5ce1fbc2072c353fbc0f139a7035e85a
Danila Rose <danila.rose.web@gmail.com>
2023-04-03 14:45:53 +0300

    added preloader + work on the adaptive

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,1 +1,10 @@
-console.log('Сборка выполнилась успешно');
+
+
+
+// TODO Preloader
+
+window.addEventListener('load', function () {
+   const preloader = document.getElementById('ui-preloader')
+   preloader.classList.add('hide')
+
+})
