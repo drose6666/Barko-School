6195578992a7e3c1a1745d1eafff9c3166795ef5
Danila Rose <danila.rose.web@gmail.com>
2023-04-04 19:11:04 +0300

    the courses screen is almost ready

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,5 +1,6 @@
 
 import './module/scrollAnimation.js';
+import Swiper from 'swiper/bundle';
 
 
 // TODO Preloader
@@ -8,4 +9,40 @@ window.addEventListener('load', function () {
    const preloader = document.getElementById('ui-preloader')
    preloader.classList.add('hide')
 
+})
+
+
+// TODO Swiper Courses
+const courses = new Swiper('#courses-swiper', {
+   slidesPerView: 4,
+   slidesPerGroup: 1,
+   loop: true,
+   spaceBetween: 30,
+   centeredSlides: true,
+   speed: 800,
+   // autoplay: {
+   //    delay: 3000,
+   //    disableOnInteraction: false,
+   // },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+   },
+   // breakpoints: {
+   //    120: {
+   //       slidesPerView: 1,
+   //       slidesPerGroup: 1,
+   //       spaceBetween: 30,
+   //    },
+   //    700: {
+   //       slidesPerView: 2,
+   //       spaceBetween: 30,
+   //       slidesPerGroup: 1
+   //    },
+   //    1150: {
+   //       slidesPerView: 3,
+   //       spaceBetween: 40,
+   //       slidesPerGroup: 1
+   //    }
+   // }
 })
